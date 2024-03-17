32760a935d5bc14407df2b0a049a05b59354945f
tisenres <tisenres@gmail.com>
2024-03-17 17:06:40 +0500

    Initial Commit

---
+++ AirBnb/AirBnbApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirBnbApp.swift
+//  AirBnb
+//
+//  Created by Nastya Shaposhnikova on 17/03/24.
+//
+
+import SwiftUI
+
+@main
+struct AirBnbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
